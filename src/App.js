9820981282b9c310425c7e5148b2242a905af41b
yujinGwon <gwonyj36@gmail.com>
2024-01-14 19:33:41 +0900

    신발 데이터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import bg from "./img/bg.png";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+  console.log(shoes[0].price);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -14,18 +19,23 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      =
       <div
         className="main-bg"
         style={{ backgroundImage: "url(" + bg + ")" }}
       ></div>
       <div className="container">
         <div className="row">
+
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + "/logo192.png"} width="80%" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            {/* <img src={process.env.PUBLIC_URL + "/logo192.png"} width="80%" /> */}
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
           </div>
+
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes2.jpg"
@@ -34,6 +44,7 @@ function App() {
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
+
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
@@ -42,6 +53,7 @@ function App() {
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
+
         </div>
       </div>
     </div>
