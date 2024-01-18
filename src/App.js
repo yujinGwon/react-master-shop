87ed971140d6c2a032221aed374c5cffc436e12b
yujinGwon <gwonyj36@gmail.com>
2024-01-18 22:40:36 +0900

    navigate, nested routes, outlet

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { useState } from "react";
 import data from "./data.js";
 import img from "./image.js";
 
-import { Routes, Route, Link } from "react-router-dom";
-import Detail from "./components/Detail.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail.js";
 import Main from "./components/Main.js";
 
 function App() {
   let [shoes] = useState(data);
   let [images] = useState(img);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -19,14 +20,23 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Cart
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
 
       <Routes>
         <Route
@@ -34,7 +44,21 @@ function App() {
           element={<Main bg={bg} shoes={shoes} images={images} />}
         />
         <Route path="/detail" element={<Detail />} />
+        {/* <Route path="*" element={<div>없는페이지요</div>} /> */}
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치임</div>} />
+        </Route>
       </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
