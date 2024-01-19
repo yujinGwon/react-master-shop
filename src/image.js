4265a4667231d55f2e90d8da52939642663facd6
yujinGwon <gwonyj36@gmail.com>
2024-01-20 00:42:45 +0900

    라이프사이클과 유즈이펙트1

--- src/image.js
+++ src/image.js
@@ -1,18 +1,18 @@
 let images = [
   {
     id: 1,
-    src: "https://codingapple1.github.io/shop/shoes1.jpg"
+    src: "https://codingapple1.github.io/shop/shoes1.jpg",
   },
 
   {
     id: 2,
-    src: "https://codingapple1.github.io/shop/shoes2.jpg"
+    src: "https://codingapple1.github.io/shop/shoes2.jpg",
   },
-  
+
   {
     id: 3,
-    src: "https://codingapple1.github.io/shop/shoes3.jpg"
-  }
+    src: "https://codingapple1.github.io/shop/shoes3.jpg",
+  },
 ];
 
 export default images;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-
-let YellowBtn = styled.button`
-  background: ${(props) => props.bg};
-  color: ${(props) => (props.bg === "blue" ? "white" : "black")};
-  padding: 10px;
-`;
-
-// let Box = styled.div`
-//   background: grey;
-//   padding: 20px;
-// `;
-
-// let NewBtn = styled.button(YellowBtn)`
-
-// `;
 
 function Detail(props) {
+  // useEffect 안에 있는 내용은 html 렌더링 후에 동작
+  // 주로 어려운 연산, 서버에서 데이터가져오는 작업, 타이머 장착하는거 등
+  useEffect(() => {});
+
+  setTimeout(() => {}, 1000);
+
+  let [count, setCount] = useState(0);
+
   let { id } = useParams();
   let 찾은상품 = props.shoes.find((x) => x.id == id);
 
   return (
     <div className="container">
-      <YellowBtn bg="yellow">버튼</YellowBtn>
-      <YellowBtn bg="orange">버튼</YellowBtn>
-      <YellowBtn bg="blue">버튼</YellowBtn>
-
+      <div className="alert alert-warning">2초이내 구매시 할인</div>
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className="row">
         <div className="col-md-6">
           <img src={찾은상품.image} alt="" width="100%" />
